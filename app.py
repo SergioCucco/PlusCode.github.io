45bc31ce036d0a1bc2113f017ccf4b3afa46066b
omeldith <melany.apaza@gmail.com>
2023-07-11 10:18:31 -0300

    URLs arregladas

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 """
-Hay que instalar en la terminal lo siguiente: 
+Hay que instalar en la terminal lo siguiente:
 pip install pillow
 pip install matplotlib
 """
 import sqlite3
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, jsonify, request, send_file, url_for
 from flask_cors import CORS
 
 DATABASE = "cursos_inventario.db"
@@ -42,23 +42,23 @@ create_database()
 
 class Curso:
     def __init__(self, codigo, nombre, cupos, precio, duracion, imagen_ruta):
-        self.codigo = codigo           
-        self.nombre = nombre           
-        self.cupos = cupos             
-        self.precio = precio           
+        self.codigo = codigo
+        self.nombre = nombre
+        self.cupos = cupos
+        self.precio = precio
         self.duracion = duracion
         self.imagen_ruta = imagen_ruta
-    
+
     def modificar(self, nuevo_nombre, nuevo_cupos, nuevo_precio, nueva_duracion, nueva_imagen_ruta):
         self.nombre = nuevo_nombre              # Modifica la descripción
         self.cupos = nuevo_cupos                # Modifica la cantidad
         self.precio = nuevo_precio              # Modifica el precio
         self.duracion = nueva_duracion
         self.imagen_ruta = nueva_imagen_ruta
-    
+
     def obtener_imagen_ruta(self):
         return self.imagen_ruta
-    
+
     def mostrar_imagen(self):
         return send_file(self.imagen_ruta, mimetype='image/jpg')
 
@@ -74,34 +74,30 @@ class Inventario:
         producto_existente = self.consultar_curso(codigo)
         if producto_existente:
             return jsonify({'message': 'Ya existe curso con el código ingresado'}), 400
-        sql = f"INSERT INTO cursos VALUES({codigo}, '{nombre}', {cupos}, {precio}, {duracion}, '{imagen_ruta}';)"
+        sql = f"INSERT INTO cursos VALUES({codigo}, '{nombre}', {cupos}, {precio}, {duracion}, '{imagen_ruta}');"
         self.cursor.execute(sql)
         self.conexion.commit()
         return jsonify({'message': 'Curso agregado satisfactoriamente'}), 200
         # nuevo_curso = Curso(codigo, nombre, cupos, precio, duracion, imagen_ruta)
         # self.cursos.append(nuevo_curso)  # Agrega un nuevo producto a la lista
-    
+
     # Método para obtener objetos de la clase "Curso" y agregarlos al inventario. Es la "R" (read) de CRUD
     def consultar_curso(self, codigo):
-        sql = f"SELECT * FROM cursos WHERE codigo = {codigo}:"
+        sql = f"SELECT * FROM cursos WHERE codigo = {codigo};"
         self.cursor.execute(sql)
         row = self.cursor.fetchone()
         if row:
             codigo, nombre, cupos, precio, duracion, imagen_ruta = row
             return Curso(codigo, nombre, cupos, precio, duracion, imagen_ruta)
         return None
-        # for curso in self.cursos:
-        #     if curso.codigo == codigo:
-        #         return curso # Retorna un objeto
-        # return False
-    
+
     # Método para modificar objetos de la clase "Curso" y agregarlos al inventario. Es la "U" (update) de CRUD
     def modificar_curso(self, codigo, nuevo_nombre, nuevo_cupos, nuevo_precio, nueva_duracion, nueva_imagen_ruta):
         curso = self.consultar_curso(codigo)
         if curso:
             # curso.modificar(nuevo_nombre,nuevo_cupos, nuevo_precio, nueva_duracion, nueva_imagen_ruta)
             curso.modificar(nuevo_nombre, nuevo_cupos, nuevo_precio, nueva_duracion, nueva_imagen_ruta)
-            sql = f'UPDATE cursos SET nombre = "{nuevo_nombre}", cupos = {nuevo_cupos}, precio = {nuevo_precio}, duracion = {nueva_duracion}, imagen_ruta = "{nueva_imagen_ruta}";'
+            sql = f'UPDATE cursos SET nombre = "{nuevo_nombre}", cupos = {nuevo_cupos}, precio = {nuevo_precio}, duracion = {nueva_duracion}, imagen_ruta = "{nueva_imagen_ruta}" WHERE codigo = {codigo};'
             self.cursor.execute(sql)
             self.conexion.commit()
             return jsonify({'message': 'Curso modificado satisfactoriamente.'}), 200
@@ -109,22 +105,12 @@ class Inventario:
 
     # Método para eliminar objetos de la clase "Curso" y agregarlos al inventario. Es la "D" (delete) de CRUD
     def eliminar_curso(self, codigo):
-        sql = f'DELETE FROM cursos WHERE codigo = {codigo}'
+        sql = f'DELETE FROM cursos WHERE codigo = {codigo};'
         self.cursor.execute(sql)
         if self.cursor.rowcount > 0:
             self.conexion.commit()
             return jsonify({'message': 'Curso eliminado satisfactoriamente.'}), 200
         return jsonify({'message': 'Curso no encontrado.'}), 404
-        # eliminar = False
-        # for curso in self.cursos:
-        #     if curso.codigo == codigo:
-        #         eliminar = True
-        #         curso_eliminar = curso      
-        # if eliminar == True:
-        #     self.cursos.remove(curso_eliminar)
-        #     print(f'Curso {codigo} eliminado.')
-        # else:
-        #     print(f'Curso {codigo} no encontrado.')
 
     def listar_cursos(self):
         self.cursor.execute('SELECT * FROM cursos')
@@ -132,25 +118,17 @@ class Inventario:
         cursos = []
         for row in rows:
             codigo, nombre, cupos, precio, duracion, imagen_ruta = row
+            ruta_relativa = '.' + url_for('static', filename=imagen_ruta)
             curso = {
             'codigo': codigo,
             'nombre': nombre,
             'cupos': cupos,
             'precio': precio,
             'duracion': duracion,
-            'imagen_ruta': imagen_ruta
+            'imagen_ruta': ruta_relativa
             }
             cursos.append(curso)
         return jsonify(cursos), 200
-        # print("#" * 50)
-        # print()
-        # print(f"Lista de cursos en el inventario:\n")
-        # print("Cod\tNombre del curso\tCupos disponibles\tPrecio\tDuracion\tRuta imagen")
-        # for curso in self.cursos:
-        #     print(f'{curso.codigo}\t\t{curso.nombre} |\t{curso.cupos} |\t${curso.precio} |\t{curso.duracion} |\t{curso.imagen.filename}')
-        # curso.mostrar_imagen()
-        # print()
-        # print("#" * 50)
 
 
 class Carrito:
@@ -158,75 +136,65 @@ class Carrito:
         self.conexion = obtener_db_conexion()
         self.cursor = self.conexion.cursor()
         self.items = []
-    
-    def agregar(self, codigo, inventario):
+
+    def agregar(self, codigo, cupos, inventario):
         curso = inventario.consultar_curso(codigo)
         if curso is None:
             return jsonify({'message': 'El curso no existe.'}), 404
-        
+        if curso.cupos < cupos:
+            return jsonify({'message': 'No hay más cupos en el curso'}), 400
         for curso_existente in self.items:
             if curso_existente.codigo == codigo:
-                return jsonify({'message': 'El curso ya está en tu carrito.'}, 400)
-        
+                return jsonify({'message': 'El curso ya está en tu carrito.'}), 400
+
         if curso.cupos > 0:
             nuevo_curso = Curso(codigo, curso.nombre, curso.cupos, curso.precio, curso.duracion, curso.imagen_ruta)
             self.items.append(nuevo_curso)
-            sql = f'UPDATE cursos SET cupos -= 1 WHERE codigo = {codigo};'
+            sql = f'UPDATE cursos SET cupos =  cupos - {cupos} WHERE codigo = {codigo};'
             self.cursor.execute(sql)
             self.conexion.commit()
             return jsonify({'message': 'Curso agregado al carrito.'}), 200
-        
-        # if curso:
-        #     for curso_existente in self.items:
-        #         if curso_existente.codigo == codigo:
-        #             print("El curso ya está en tu carrito")
-        #             return
-        #     if curso.cupos > 0:
-        #         self.items.append(curso)
-        #         print(f"El curso {curso.nombre} se ha agregado al carrito")
-        #         curso.cupos -= 1
-        #     else:
-        #         print(f"No hay cupos disponibles para {curso.nombre}")
-        # else:
-        #     print("Curso no disponible")
-    
-    def sacar(self, codigo, inventario):
+
+
+    def sacar(self, codigo, cupos, inventario):
         for curso in self.items:
             if curso.codigo == codigo:
-                curso.cupos += 1
+                # curso.cupos += 1
                 self.items.remove(curso)
-                sql = f'UPDATE cursos SET cupos += 1 WHERE codigo = {codigo};'
+                sql = f'UPDATE cursos SET cupos = cupos + {cupos} WHERE codigo = {codigo};'
                 self.cursor.execute(sql)
                 self.conexion.commit()
                 return jsonify({'message': 'Curso sacado'}), 200
-        
+
         return jsonify({'message': 'El curso no se encuentra en el carrito.'}), 404
 
-    
+
     # def vaciar_carrito(self):
     #     self.items = []
     #     print("El carrito se ha vaciado.")
-    
+
     def mostrar_carrito(self):
         productos_carrito = []
         for curso in self.items:
-            curso = {'codigo': curso.codigo, 'nombre': curso.nombre, 'cupos': curso.cupos, 'precio': curso.precio, 'duracion': curso.duracion, 'Ruta imagen': curso.imagen_ruta}
-            productos_carrito.append(curso)
+            curso_data = {
+                'codigo': curso.codigo,
+                'nombre': curso.nombre,
+                'cupos': curso.cupos,
+                'precio': curso.precio,
+                'duracion': curso.duracion,
+                'imagen_ruta': curso.imagen_ruta
+            }
+            productos_carrito.append(curso_data)
         return jsonify(productos_carrito), 200
-        # if self.items:
-        #     print(f"Cursos en el carrito".center(30, "-"))
-        #     for curso in self.items:
-        #         print(f"- {curso.nombre}")
-        # else:
-        #     print("El carrito esta vacio")
-    
+
     def calcular_total(self):
         total = 0
         for curso in self.items:
             total += curso.precio
         return total
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./static')
+app.config['STATIC_URL_PATH'] = './static'
 CORS(app, origins='*')
 
 carrito = Carrito()
@@ -269,7 +237,7 @@ def modificar_curso(codigo):
     nueva_imagen_ruta = request.json.get('imagen_ruta')
     return inventario.modificar_curso(codigo, nuevo_nombre, nuevo_cupos, nuevo_precio, nueva_duracion, nueva_imagen_ruta)
 
-@app.route('/cursos/<int:codigo>', methods='DELETE')
+@app.route('/cursos/<int:codigo>', methods=['DELETE'])
 def eliminar_curso(codigo):
     return inventario.eliminar_curso(codigo)
 
@@ -280,14 +248,16 @@ def index():
 @app.route('/carrito', methods=['POST'])
 def agregar_carrito():
     codigo = request.json.get('codigo')
+    cupos = request.json.get('cupos')
     inventario = Inventario()
-    return carrito.agregar(codigo, inventario)
+    return carrito.agregar(codigo, cupos, inventario)
 
 @app.route('/carrito', methods=['DELETE'])
 def sacar_carrito():
     codigo = request.json.get('codigo')
+    cupos = request.json.get('cupos')
     inventario = Inventario()
-    return carrito.sacar(codigo, inventario)
+    return carrito.sacar(codigo, cupos, inventario)
 
 @app.route('/carrito', methods=['GET'])
 def obtener_carrito():
